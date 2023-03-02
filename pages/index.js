ebe240017220b70dc5a10423d7aea9bab0f66a5a
unknown <eodrmfdl1004@naver.com>
2023-03-02 17:09:01 +0900

    [Add] 페이지 구축

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import FeaturedPosts from "@/components/home/FeaturedPosts/FeaturedPosts";
+import Hero from "@/components/home/Hero/Hero";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
