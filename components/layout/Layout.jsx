f0ad83ed016c53337675ee18bf17e380e37f37c5
unknown <eodrmfdl1004@naver.com>
2023-03-02 17:24:09 +0900

    [Add] 헤더 컴포넌트 추가

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainNavigation from "./MainNavigation/MainNavigation";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <MainNavigation />
+      <main>{children}</main>
+    </>
+  );
+}
