57c3ebd436a33c6fe4d9a182c98414faf54f73e9
unknown <eodrmfdl1004@naver.com>
2023-03-03 12:45:55 +0900

    [Feat] 마크다운 이미지 불러오기

--- components/posts/_detail/PostContent/PostContent.jsx
+++ components/posts/_detail/PostContent/PostContent.jsx
@@ -2,14 +2,50 @@ import React from "react";
 import styles from "./PostContent.module.css";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
+import Image from "next/image";
 import PostHeader from "../PostHeader/PostHeader";
 
 export default function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenders = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+              layout='responsive'
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenders}>{post.content}</ReactMarkdown>
     </article>
   );
 }
