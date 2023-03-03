3e281c7fb6c7a43b99cd614f1259b04be5d60bb5
unknown <eodrmfdl1004@naver.com>
2023-03-03 12:17:50 +0900

    [Feat] 동적 페이지 데이터 사전 렌더링

--- components/posts/_detail/PostContent/PostContent.jsx
+++ components/posts/_detail/PostContent/PostContent.jsx
@@ -4,20 +4,12 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 import PostHeader from "../PostHeader/PostHeader";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2023-03-02",
-  content: "# 첫 번째 게시글 입니다.",
-};
-
-export default function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+export default function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={styles.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
