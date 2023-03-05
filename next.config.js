5f158c11c2a6898b4d321a454ab314d0841920af
unknown <eodrmfdl1004@naver.com>
2023-03-05 19:17:54 +0900

    [Add] 환경변수 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: "ez1n",
+        mongodb_password: "yYosMi1CkYjsk1Fe",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-blog-dev",
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: "ez1n",
+      mongodb_password: "yYosMi1CkYjsk1Fe",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-blog",
+    },
+  };
+};
