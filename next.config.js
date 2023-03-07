8656bb3d6965055cc40057cf58933e3d54ff2b35
unknown <eodrmfdl1004@naver.com>
2023-03-07 13:25:52 +0900

    [Fix] credentials 수정

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      env: {
-        mongodb_username: "ez1n",
-        mongodb_password: "yYosMi1CkYjsk1Fe",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "my-blog-dev",
-      },
-    };
-  }
-
-  return {
-    env: {
-      mongodb_username: "ez1n",
-      mongodb_password: "yYosMi1CkYjsk1Fe",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "my-blog",
-    },
-  };
+module.exports = {
+  reactStrictMode: true,
 };
